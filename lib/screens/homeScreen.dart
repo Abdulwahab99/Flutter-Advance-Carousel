49902945a07e5e196f217d14d78d6d78d86987a7
Anwar Abir <abuanwar072@gmail.com>
2019-11-20 17:27:37 +0600

    animation done

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:advance_carousal/constant/data.dart';
 import 'package:flutter/material.dart';
 
 final imagesList = [
@@ -9,7 +10,7 @@ final imagesList = [
 final colorList = [
   Colors.redAccent.shade100,
   Colors.blueAccent.shade100,
-  Colors.amber.shade50
+  Colors.green
 ];
 
 class HomeScreen extends StatefulWidget {
@@ -18,14 +19,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int currentPage = 1;
+  int currentPage = 0;
   PageController _pageController;
 
   @override
   void initState() {
     super.initState();
     _pageController =
-        PageController(initialPage: currentPage, viewportFraction: .7);
+        PageController(initialPage: currentPage, viewportFraction: 0.7);
   }
 
   @override
@@ -36,38 +37,140 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    print(currentPage);
-    return Scaffold(
-      body: PageView.builder(
-        controller: _pageController,
-        pageSnapping: true,
-        onPageChanged: _pageChange,
-        itemBuilder: (context, index) {
-          return iteamBuilder(index);
-        },
-        itemCount: 3,
-      ),
+    return SafeArea(
+      child: Scaffold(
+          body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * .7,
+            child: PageView.builder(
+              controller: _pageController,
+              pageSnapping: true,
+              onPageChanged: _pageChange,
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return iteamBuilder(index);
+              },
+            ),
+          ),
+          details(currentPage)
+        ],
+      )),
+    );
+  }
+
+  Widget details(index) {
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (context, child) {
+        double value = 1;
+        if (_pageController.position.haveDimensions) {
+          value = (_pageController.page - index);
+          value = (1 - (value.abs() * 1)).clamp(0.0, 1.0);
+        }
+        print(value);
+        return Expanded(
+          child: Transform.translate(
+            offset: Offset(
+                0, 0 - (value - 1) * MediaQuery.of(context).size.height * .3),
+            child: Opacity(
+              opacity: value,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "${detailsList[index].title}",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "${detailsList[index].description}",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 2,
+                    width: 80,
+                    color: colorList[index],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5),
+                    child: Text(
+                      "Read More",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: colorList[index],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 
   Widget iteamBuilder(index) {
-    return Align(
-      alignment: Alignment.topCenter,
-      child: Container(
-        height: MediaQuery.of(context).size.height * .7,
-        // width: MediaQuery.of(context).size.width * .6,
-        margin: EdgeInsets.only(left: 30, right: 30, bottom: 10),
-        color: Colors.grey.shade400,
-        child: Material(
-          elevation: 4.0,
-          shape: RoundedRectangleBorder(
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (context, child) {
+        double value = 1;
+        if (_pageController.position.haveDimensions) {
+          value = (_pageController.page - index);
+          value = (1 - (value.abs() * .3)).clamp(0.0, 1.0);
+
+          return Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              height: Curves.easeIn.transform(value) *
+                  MediaQuery.of(context).size.height *
+                  .7,
+              margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: child,
+            ),
+          );
+        } else {
+          // We use this for the 1st time the app run then the _pageController.position.haveDimensions = false
+          return Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              height: Curves.easeIn
+                      .transform(index == currentPage ? value : value * .7) *
+                  MediaQuery.of(context).size.height *
+                  .7,
+              margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: child,
+            ),
+          );
+        }
+      },
+      child: Material(
+        elevation: 4.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(15),
+              bottomRight: Radius.circular(15)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+          child: ClipRRect(
             borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(15),
-                bottomRight: Radius.circular(15)),
-          ),
-          child: Image.asset(
-            imagesList[index],
-            fit: BoxFit.fill,
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+            child: Image.asset(
+              imagesList[index],
+              fit: BoxFit.fitHeight,
+            ),
           ),
         ),
       ),
